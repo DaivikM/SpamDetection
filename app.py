6c12684343e431646bc354608efa6d9d5e7a3a5c
DaivikM <110714831+DaivikM@users.noreply.github.com>
2025-04-19 09:57:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,60 +2,68 @@
 """
 Created on Fri Aug 25 01:27:53 2023
 
-@author: daivi
+@author: DaivikM
 """
 
 import streamlit as st
 import pickle
 import string
+import os
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import nltk
+from nltk.tokenize import TreebankWordTokenizer  # Using safer tokenizer
 
-# Set the NLTK data path to a location where your app can access it
-#nltk.data.path.append("/path/to/nltk_data")
+# Create and use a local nltk_data folder in a portable way
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_path, exist_ok=True)
+nltk.data.path.append(nltk_data_path)
 
-# Download the Punkt tokenizer resource if not already downloaded
+# Download necessary resources
+for resource in ['punkt', 'stopwords']:
+    try:
+        nltk.data.find(resource)
+    except LookupError:
+        nltk.download(resource, download_dir=nltk_data_path)
+
 try:
-    nltk.data.find('tokenizers/punkt')
+    nltk.data.find('corpora/stopwords')
 except LookupError:
-    nltk.download('punkt')
-
-try:
-    nltk.data.find('corpora\stopwords')
-except LookupError:
-    nltk.download('stopwords')
+    nltk.download('stopwords', download_dir=nltk_data_path)
 
 ps = PorterStemmer()
+tokenizer = TreebankWordTokenizer()
 
 def transform_text(text):
     text = text.lower()
-    text = nltk.word_tokenize(text)
-    
-    y=[]
+    text = tokenizer.tokenize(text)  # Replaces nltk.word_tokenize to avoid punkt issues
+
+    y = []
     for i in text:
         if i.isalnum():
             y.append(i)
-            
+
     text = y[:]
     y.clear()
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
-            
+
     text = y[:]
     y.clear()
     for i in text:
         y.append(ps.stem(i))
-            
+
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+# Load vectorizer and model
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
+# Streamlit App UI
 st.title("Email/SMS Text Classifier")
 
-input_sms = st.text_area("Enter your message: ")
+input_sms = st.text_area("Enter your message:")
 
 if st.button('Predict'):
     # 1. preprocess
@@ -67,8 +75,5 @@ if st.button('Predict'):
     # 3. predict
     result = model.predict(vector_input)[0]
 
-    # 4. display
-    if result==1:
-        st.header("Spam")
-    else:
-        st.header("Not Spam")
+    # 4. display result
+    st.header("Spam" if result == 1 else "Not Spam")
